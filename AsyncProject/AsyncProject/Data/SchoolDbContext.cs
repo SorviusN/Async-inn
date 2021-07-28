28c89321ba3121c1c03b8ce74b7299552a539082
SorviusN <Opt1206@gmail.com>
2021-07-27 18:01:48 -0700

    Scaffolding complete

---
+++ AsyncProject/AsyncProject/Data/SchoolDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AsyncProject.Models;
+
+namespace AsyncProject.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public SchoolDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // This call the base method, but does nothing.
+            // base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>().HasData(
+                new Student { Id = 1, FirstName = "Aman", LastName = "Almeida" },
+                new Student { Id = 2, FirstName = "Hannah", LastName = "Larson" },
+                new Student { Id = 3, FirstName = "Melanie", LastName = "Campbell" }
+                );
+        }
+    }
+}
